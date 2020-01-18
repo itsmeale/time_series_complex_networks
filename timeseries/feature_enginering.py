c482b1825add6299afa95c25378a7d171a5605f8
Ale Farias <mail.alefarias@gmail.com>
2020-01-18 11:27:37 -0200

    Adds features f5 and f6

--- timeseries/feature_enginering.py
+++ timeseries/feature_enginering.py
@@ -21,9 +21,7 @@ def gradient(dataframe, over):
 
 	for i in range(len(temp_df)):
 		if i-1 < 0 or np.isnan(temp_df.loc[i-1, over]): continue
-		temp_df.loc[i, new_col] = (
-			temp_df.loc[i, over] / temp_df.loc[i-1, over] - 1
-		)
+		temp_df.loc[i, new_col] = (temp_df.loc[i, over] / temp_df.loc[i-1, over] - 1)
 
 	return temp_df[new_col]
 
@@ -33,12 +31,15 @@ original_series = pd.read_csv(DATA_FOLDER.format(RAW_DATA_FILE), sep="\t")
 original_series.columns = ["date", "cp"]
 original_series["d"] = original_series.index + 1  # starts the index from 1
 original_series["cpj"] = original_series["cp"].rolling(window=J).mean()
+
 original_series[MAq_short] = original_series.cpj.rolling(Q_short).mean()
 original_series[MAq_long] = original_series.cpj.rolling(Q_long).mean()
 
-original_series["f1"] = (original_series.cpj / original_series[MAq_short]) - 1
-original_series["f2"] = (original_series.cpj / original_series[MAq_long]) - 1
+original_series["f1"] = original_series.cpj / original_series[MAq_short] - 1
+original_series["f2"] = original_series.cpj / original_series[MAq_long] - 1
 original_series["f3"] = gradient(original_series, over=MAq_short)
 original_series["f4"] = gradient(original_series, over=MAq_long)
+original_series["f5"] = (original_series.cpj - original_series.cpj.rolling(window=Q_short).min()) / (original_series.cpj.rolling(window=Q_short).max() - original_series.cpj.rolling(window=Q_short).min())
+original_series["f6"] = (original_series.cpj - original_series.cpj.rolling(window=Q_long).min()) / (original_series.cpj.rolling(window=Q_long).max() - original_series.cpj.rolling(window=Q_long).min())
 
 original_series.to_csv("temp.csv", index=False)
